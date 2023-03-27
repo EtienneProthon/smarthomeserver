5468ecfd79f34ff85b1f270fbc91a465607bb560
Etienne Prothon <etienne.prothon@mypl.io>
2023-03-27 22:11:21 +0200

    fix: mosquitto config folder creation

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 source .env
 sudo mkdir -p $DATADIR/loki/config && sudo cp config/loki-config.yaml $DATADIR/loki/config/loki-config.yaml
 sudo mkdir -p $DATADIR/prometheus/etc && sudo cp config/prometheus_config.yaml $DATADIR/prometheus/etc/prometheus.yml
-sudo mkdir -p $DATADIR/mosquitto && sudo cp config/mosquitto.conf $DATADIR/mosquitto/config/mosquitto.conf
+sudo mkdir -p $DATADIR/mosquitto/config && sudo cp config/mosquitto.conf $DATADIR/mosquitto/config/mosquitto.conf
 sudo mkdir -p $DATADIR/zigbee2mqtt/data && sudo cp config/zigbee2mqtt_configuration.yaml $DATADIR/zigbee2mqtt/data/configuration.yaml
