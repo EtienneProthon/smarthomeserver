4207609d09cea4791a03e2113ec61745d37780e6
Etienne Prothon <etienne.prothon@mypl.io>
2023-04-02 01:26:15 +0200

    feat: create all mosquitto folder from setup

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ mkdir -p $DATADIR/grafana/data
 mkdir -p $DATADIR/homepage && cp config/homepage-docker.yaml $DATADIR/homepage/docker.yaml
 mkdir -p $DATADIR/loki/data && mkdir -p $DATADIR/loki/config && cp config/loki-config.yaml $DATADIR/loki/config/loki-config.yaml
 mkdir -p $DATADIR/prometheus/etc && mkdir -p $DATADIR/prometheus/data && cp config/prometheus_config.yaml $DATADIR/prometheus/etc/prometheus.yml
-mkdir -p $DATADIR/mosquitto/config && cp config/mosquitto.conf $DATADIR/mosquitto/config/mosquitto.conf
+mkdir -p $DATADIR/mosquitto/config && mkdir -p $DATADIR/mosquitto/log && mkdir -p $DATADIR/mosquitto/data && cp config/mosquitto.conf $DATADIR/mosquitto/config/mosquitto.conf
 mkdir -p $DATADIR/zigbee2mqtt/data && cp config/zigbee2mqtt_configuration.yaml $DATADIR/zigbee2mqtt/data/configuration.yaml
